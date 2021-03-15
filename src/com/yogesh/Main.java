fef7a108e660bbc4ecdeeae4527bed16e64b11d7
Yogesh <you@example.com>
2021-03-15 22:21:38 +0530

    InsertionSort Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,13 +1,13 @@
 package com.yogesh;
 
-import com.yogesh.SortingAlgorithms.SelectionSort;
+import com.yogesh.SortingAlgorithms.InsertionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = { 7, 4, 3, 8, 9, 1 };
-        var sorter = new SelectionSort();
+        var sorter = new InsertionSort();
         sorter.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
