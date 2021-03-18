679a343e8fc10605acfa594478aa6beb46c0f19b
Yogesh <you@example.com>
2021-03-18 21:51:55 +0530

    LinearSearch Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,15 +1,15 @@
 package com.yogesh;
 
+import com.yogesh.SearchingAlgorithms.LinearSearch;
 import com.yogesh.SortingAlgorithms.BucketSort;
-import com.yogesh.SortingAlgorithms.CountingSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { };
-        var sorter = new BucketSort();
-        sorter.sort(arr, 3);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = { 9, 8, 9, 7, 6, 5, 4 };
+        var search = new LinearSearch();
+        var index = search.linearSearch(arr, 9);
+        System.out.println(index);
     }
 }
