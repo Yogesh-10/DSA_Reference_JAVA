c9853078559ace98a7c03e2dc0ac4ee4e468fa38
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-03-18 20:33:07 +0530

    CountingSort Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,14 +1,14 @@
 package com.yogesh;
 
-import com.yogesh.SortingAlgorithms.QuickSort;
+import com.yogesh.SortingAlgorithms.CountingSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 4 };
-        var sorter = new QuickSort();
-        sorter.sort(arr);
+        int[] arr = { 4, 1, 3, 0, 4 };
+        var sorter = new CountingSort();
+        sorter.sort(arr, 4);
         System.out.println(Arrays.toString(arr));
     }
 }
