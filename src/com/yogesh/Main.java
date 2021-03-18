dc4b37936492a7b403b9181f06e6b8cf04552281
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-03-19 00:13:13 +0530

    JumpSearch Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,13 +1,14 @@
 package com.yogesh;
 
 import com.yogesh.SearchingAlgorithms.BinarySearch;
+import com.yogesh.SearchingAlgorithms.JumpSearch;
 import com.yogesh.SearchingAlgorithms.TernarySearch;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {  };
-        var search = new TernarySearch();
-        var index = search.ternarySearch(arr, 9);
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        var search = new JumpSearch();
+        var index = search.jumpSearch(arr, 6);
         System.out.println(index);
     }
 }
