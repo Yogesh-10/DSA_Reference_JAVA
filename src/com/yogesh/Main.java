4a9bbf2b659f807807712b4b516599ad274f04a7
Yogesh <you@example.com>
2021-03-18 22:06:09 +0530

    TernarySearch Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,15 +1,12 @@
 package com.yogesh;
 
-import com.yogesh.SearchingAlgorithms.LinearSearch;
-import com.yogesh.SortingAlgorithms.BucketSort;
-
-import java.util.Arrays;
+import com.yogesh.SearchingAlgorithms.BinarySearch;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 9, 8, 9, 7, 6, 5, 4 };
-        var search = new LinearSearch();
-        var index = search.linearSearch(arr, 9);
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var search = new BinarySearch();
+        var index = search.binarySearchIterative(arr, 1);
         System.out.println(index);
     }
 }
