bbdded899d854a88dbffe30563a7db3d6e2e54d4
Yogesh <you@example.com>
2021-03-18 20:48:39 +0530

    BucketSort Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -1,14 +1,15 @@
 package com.yogesh;
 
+import com.yogesh.SortingAlgorithms.BucketSort;
 import com.yogesh.SortingAlgorithms.CountingSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 4, 1, 3, 0, 4 };
-        var sorter = new CountingSort();
-        sorter.sort(arr, 4);
+        int[] arr = { };
+        var sorter = new BucketSort();
+        sorter.sort(arr, 3);
         System.out.println(Arrays.toString(arr));
     }
 }
