3a8d4c01eab2376b62bff2691b294c4b823d300a
Yogesh <you@example.com>
2021-03-19 15:20:07 +0530

    String Algorithms Implemented

--- src/com/yogesh/Main.java
+++ src/com/yogesh/Main.java
@@ -4,7 +4,7 @@ import com.yogesh.StringManipulation_Algorithms.StringUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int count = StringUtils.countVowels(null);
-        System.out.println(count);
+        var reversed = StringUtils.isPalindrome("ABBA");
+        System.out.println(reversed);
     }
 }
