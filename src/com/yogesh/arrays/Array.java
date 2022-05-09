ad4714a9db38e8b88e963a847761dcaa29b65955
Yogesh C <71348279+Yogesh-10@users.noreply.github.com>
2022-05-09 22:15:53 +0530

    Update Array.java

--- src/com/yogesh/arrays/Array.java
+++ src/com/yogesh/arrays/Array.java
@@ -47,6 +47,70 @@ public class DynamicArray {
 
         count--;
     }
+    
+    public void insertAt(int item, int index) {
+    if (index < 0 || index > count)
+      throw new IllegalArgumentException();
+
+    // Note that I've extracted the logic for
+    // resizing the array into this private
+    // method so we can reuse in insert() and
+    // insertAt() methods.
+    //
+    // This also made our code cleaner and
+    // more readable.
+    resizeIfRequired();
+
+    for (int i = count - 1; i >= index; i--)
+      items[i + 1] = items[i];
+
+    items[index] = item;
+    count++;
+  }
+
+  private void resizeIfRequired() {
+    if (items.length == count) {
+      int[] newItems = new int[count * 2];
+
+      for (int i = 0; i < count; i++)
+        newItems[i] = items[i];
+
+      items = newItems;
+    }
+  }
+
+  public void reverse() {
+    int[] newItems = new int[count];
+
+    for (int i = 0; i < count; i++)
+      newItems[i] = items[count - i - 1];
+
+    items = newItems;
+  }
+
+  public int max() {
+    // O(n): Because we have to iterate over
+    // the entire array to find the largest
+    // number. This number may be at the end
+    // of the array (worst case scenario).
+    int max = 0;
+    for (int item : items)
+      if (item > max)
+        max = item;
+
+    return max;
+  }
+
+  public Array intersect(Array other) {
+    var intersection = new Array(count);
+
+    for (int item : items)
+      if (other.indexOf(item) >= 0)
+        intersection.insert(item);
+
+    return intersection;
+  }
+
 
     public void print(){
         for(int i = 0; i < count; i++)
